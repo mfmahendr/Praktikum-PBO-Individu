a42af30a48cffdfe09488e0bcd042c85cd38f1dd
M. Fadhil Mahendra <71614957+mfmahendr@users.noreply.github.com>
2021-04-23 11:39:40 +0700

    Menambahkan File Praktikum Chapter 4

---
+++ Chapter 4/Chapter 4/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chapter_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTukar_Click(object sender, EventArgs e)
+        {
+            QRManager.TukarInfo(ref tbNamaPenerima, ref tbNamaPengirim);
+            QRManager.TukarInfo(ref tbAlamatAsal, ref tbAlamatTujuan);
+            QRManager.TukarInfo(ref tbHpPenerima, ref tbHpPengirim);
+        }
+    }
+}
